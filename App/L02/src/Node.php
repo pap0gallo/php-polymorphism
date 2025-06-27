840d5fb76290801622eaac1c34ac7766fa09594c
pap0gallo <khodunov14@gmail.com>
2025-06-27 18:09:01 +0300

    solution for lesson 02

--- App/L02/src/Node.php
+++ App/L02/src/Node.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\src;
+namespace App\L02\src;
 
 class Node
 {
     private ?self $next = null;
     private $value = null;
 
-    public function __construct($value, Node $node = null)
+    public function __construct($value, ?Node $node = null)
     {
         $this->next = $node;
         $this->value = $value;
