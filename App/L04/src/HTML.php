0bf08cf345775465ca4c36ad60982c67aa46e3cc
pap0gallo <khodunov14@gmail.com>
2025-06-30 09:55:55 +0300

    solution for lesson 04

---
+++ App/L04/src/HTML.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\L03\src\getLinks;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+$tag = ['name' => 'hr', 'class' => 'px-3', 'id' => 'myid', 'tagType' => 'single'];
+
+// BEGIN (write your solution here)
+function stringify(array $tag): ?string
+{
+    $name = $tag['name'];
+    $tagType = $tag['tagType'];
+    $body = $tag['body'] ?? '';
+
+    $attributes = $tag;
+    unset($attributes['name'], $attributes['tagType'], $attributes['body']);
+
+    $attributesStrings = [];
+    foreach ($attributes as $key => $value) {
+        $attributesStrings [] = $key . '="' . $value . '"';
+    }
+    $attrStr = trim(implode(' ', $attributesStrings));
+    return $tagType === 'single'
+        ? "<$name $attrStr>"
+        : "<$name $attrStr>$body</$name>";
+}
+
+print_r(stringify($tag));
+// END
