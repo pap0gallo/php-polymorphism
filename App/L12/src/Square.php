df8f026e790ff87cb584c58f4f52cc3f2a3c1a06
pap0gallo <khodunov14@gmail.com>
2025-07-06 15:24:14 +0300

    solution for lesson 12

--- App/L12/src/Square.php
+++ App/L12/src/Square.php
@@ -5,5 +5,20 @@ namespace App\Square;
 use App\Dispatcher;
 
 // BEGIN (write your solution here)
+function init()
+{
+    Dispatcher\defmethod(__NAMESPACE__, 'getArea', function ($self) {
+        return $self['data']['side'] ** 2;
+    });
+}
 
+function make($side)
+{
+    return ['name' => __NAMESPACE__, 'data' => ['side' => $side]];
+}
+
+function getSide($self)
+{
+    return $self['data']['side'];
+}
 // END
